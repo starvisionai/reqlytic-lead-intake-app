b882c96581cbcaa64308a62a30c73990ddc74e90
Christopher Richard <chris@starvisionventures.com>
2026-05-20 22:22:33 +0000

    File changes

--- src/components/AppLogo.jsx
+++ src/components/AppLogo.jsx
@@ -1,14 +1,14 @@
-const LOGO_URL = 'https://media.base44.com/images/public/6a03a70d507e5b17ace04d21/238e0cb5e_IntakeIQ_Logo_v2.png';
+const LOGO_URL = 'https://media.base44.com/images/public/6a03a70d507e5b17ace04d21/ac2cff078_Main_App_Logo.png';
 
 /**
- * Renders the IntakeIQ logo image.
+ * Renders the Reqlytic logo image.
  * className: additional classes for sizing/spacing
  */
 export default function AppLogo({ className = '' }) {
   return (
     <img
       src={LOGO_URL}
-      alt="IntakeIQ"
+      alt="Reqlytic"
       style={{ mixBlendMode: 'multiply' }}
       className={`object-contain ${className}`}
     />
